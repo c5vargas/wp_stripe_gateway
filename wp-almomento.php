6e43e1e27092865c84d7beda4e7a7b9c3899e8ee
Carles Vargas <carlesvargas5@gmail.com>
2024-11-28 11:44:31 +0100

    feat: add template redirect to PWA application

--- wp-almomento.php
+++ wp-almomento.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: WP Almomento for PWA Plugin
  * Description: Este plugin te permite integrar una PWA enfocada al ecommerce utilizando tu Wordpress como API para obtener los productos.
- * Version: 2.0.7
+ * Version: 2.0.9
  * Author: Jaestic S.L
  * Text Domain: wp-almomento
  */
@@ -32,6 +32,29 @@ function sg_missing_wc_notice() {
     </div>';
 }
 
+function bbloomer_order_pay_without_login( $allcaps, $caps, $args ) {
+    if ( isset( $caps[0], $_GET['key'] ) ) {
+        if ( $caps[0] == 'pay_for_order' ) {
+            $order_id = isset( $args[2] ) ? $args[2] : null;
+            $order = wc_get_order( $order_id );
+            if ( $order ) {
+                $allcaps['pay_for_order'] = true;
+            }
+        }
+    }
+    return $allcaps;
+}
+
+function change_cod_payment_order_status( $order_status, $order ) {
+    return 'on-hold';
+}
+
+function wc_thank_you_redirect() { 
+  if( isset( $_GET['key'] ) && is_wc_endpoint_url( 'order-received' ) ) {
+    wp_redirect('https://app.almomento.cat/account/orders');
+  }
+}
+
 add_action( 'woocommerce_api_loaded', function(){
 	include_once( 'class-wc-api-custom.php' );
 });
@@ -41,4 +64,8 @@ add_filter( 'woocommerce_api_classes', function( $classes ){
 	return $classes;
 });
 
-add_action('init', 'sg_init');
+add_action( 'template_redirect', 'wc_thank_you_redirect' );
+add_filter( 'woocommerce_cod_process_payment_order_status', 'change_cod_payment_order_status', 10, 2 );
+add_filter( 'user_has_cap', 'bbloomer_order_pay_without_login', 9999, 3 );
+add_filter( 'woocommerce_order_email_verification_required', '__return_false', 9999 );
+add_action( 'init', 'sg_init' );
